d5bbd6050b6a144bb3795785a373815cee01bd6b
hadi <aalgvs8@gmail.com>
2025-08-31 21:04:35 +0330

    init14

--- routes/admin/api_key.py
+++ routes/admin/api_key.py
@@ -1,18 +1,23 @@
-from flask import request, Blueprint, Response
+from flask import request, Blueprint, jsonify
 import telebot
 from datetime import datetime
+from routes.admin.config_telbot import TOKEN
 
 api_bp = Blueprint('api_bp', __name__)
 
 @api_bp.route('/api-key')
 def api_key():
     api = request.args.get('api')
-    if api == '14b06e28-8366-11f0-b731-180373a6e88c':
+    request_type = request.args.get('request_type')
+    if api == '14b06e28-8366-11f0-b731-180373a6e88c' and request_type == 'turned_on':
         bot = telebot.TeleBot(TOKEN)
-        now = datetime.now().strftime('%M/%D %H:%M:%S')
-        bot.send_message(12315123, now)
-        return Response(status=200)
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+        bot = telebot.TeleBot(TOKEN)
+        bot.send_message(6673359808, now)
+
+        return jsonify({'status':200})
+        
     else:
-        return Response(status=404)
+        return jsonify({'status':404}),404
     
